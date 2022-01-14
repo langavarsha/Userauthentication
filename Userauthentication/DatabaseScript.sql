fb50e4c3c39a06dff8ac6a168f4084b8edeefd1c
langavarsha <90497637+langavarsha@users.noreply.github.com>
2022-01-14 23:19:09 +0530

    Rename UserAuthentication.sql to DatabaseScript.sql

---
+++ Userauthentication/DatabaseScript.sql
@@ -0,0 +1,22 @@
+﻿USE [UserAuthentication]
+GO
+/****** Object:  Table [dbo].[User]    Script Date: 01/14/2022 9:37:39 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[User](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Email] [nvarchar](max) NULL,
+	[Password] [nvarchar](max) NULL,
+ CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+SET IDENTITY_INSERT [dbo].[User] ON 
+
+INSERT [dbo].[User] ([Id], [Email], [Password]) VALUES (1, N'admin@gmail.com', N'fc0iUkg331qk3V8HY6MWvQ==')
+SET IDENTITY_INSERT [dbo].[User] OFF
+GO
