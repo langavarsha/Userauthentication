ae6faee0b6e69f1e9d9b1f17418bd321e66a2450
TimesTechno <langavarsha@gmail.com>
2022-01-14 19:33:41 +0530

    Add project files.

---
+++ Userauthentication/Controllers/HomeController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Userauthentication.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+using Userauthentication.common;
+
+namespace Userauthentication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private  DBContextClass _context;
+        private readonly string con = null;
+
+        private Common common;
+        public HomeController(DBContextClass dbclass)
+        {
+         
+           // _context = dbclass;          
+           // con = dbclass.Database.GetDbConnection().ConnectionString;
+            
+        }
+
+        public IActionResult Index()
+        {
+          
+            //  _context = new DBContextClass();
+            return View();
+        }
+        public IActionResult CreateUser()
+        {
+            string sess = HttpContext.Session.GetString("UserName");
+
+            if (sess == null)
+            {
+                return RedirectToAction("Login");
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public IActionResult UserList()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+       
+        [HttpPost]
+        public IActionResult Login(User login)
+        {
+            _context = new DBContextClass();
+                     
+            string pass = common.Encrypt(login.Password);
+
+            var loginqry = from lgn in _context.Users
+                          
+                           where lgn.Password == pass && lgn.Email == login.Email
+
+                           select new { lgn.Email};
+
+
+            if (loginqry.Count() == 0)
+            {
+                ViewBag.Message = "Invalid user name or password.";
+                return View();
+            }
+            else
+            {
+
+
+                foreach (var l in loginqry)
+                {
+                    HttpContext.Session.SetString("UserName", l.Email);
+                   
+                }
+                return RedirectToAction("Index");
+            }
+            
+
+        }
+    }
+}
---
+++ Userauthentication/Models/DBContextClass.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace Userauthentication.Models
+{
+    public class DBContextClass : DbContext
+    {
+        public DBContextClass()
+        {
+
+        }
+        //public DBContextClass(DbContextOptions<DBContextClass> options)
+        //    : base(options)
+        //{
+
+        //}
+        public virtual DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            if (!optionsBuilder.IsConfigured)
+            {
+
+                var builder = new ConfigurationBuilder()
+              .SetBasePath(Directory.GetCurrentDirectory()) // <== compile failing here
+              .AddJsonFile("appsettings.json")
+                .Build();
+                //  optionsBuilder.UseSqlServer("Server=DESKTOP-DDHP53U\\SQLEXPRESS;Database=PucDB;user id=varsha; password=sa;Trusted_Connection=True;");
+
+                //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                // optionsBuilder.UseSqlServer("Server=DESKTOP-DDHP53U\\SQLEXPRESS;Database=PucDB;user id=varsha; password=sa;Trusted_Connection=True;");
+
+
+            }
+        }
+    }
+}
